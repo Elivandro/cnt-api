8fb8e6b5fc295b25164b74aabf20fcebe155ba79
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-11-27 16:35:55 -0400

    feat: homepage configurada para ficar responsiva

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,10 @@
         }
     </style>
 </head>
-<body class="bg-gray-100 font-sans antialiased">
+<body class="bg-gray-100 font-sans antialiased overflow-x-hidden">
 <div id="app">
     <header class="bg-gray-900 py-6">
-        <div class="container mx-auto w-10/12 flex justify-between items-center px-4">
+        <div class="container mx-auto w-full lg:w-10/12 flex justify-between items-center px-4">
             <h1 class="lg:text-3xl font-bold text-green-500">Documentação da API Viacnt</h1>
             <nav>
                 <a href="#" class="text-gray-300 hover:text-gray-100 px-4">Contato</a>
@@ -24,31 +24,28 @@
     </header>
 
     <main class="container mx-auto py-12">
-        <div class="flex flex-col lg:flex-row w-10/12 justify-center" x-data="{
+        <div class="flex flex-col lg:flex-row w-full lg:w-10/12 justify-center" x-data="{
             open : true,
             show : false
             }">
-            <nav class="w-10/12 mx-auto lg:w-1/4 px-4 py-8 mr-8">
+            <nav class="w-10/12 mx-auto lg:w-1/4 px-4 py-8">
                 <ul class="flex lg:block justify-between">
                     <li class="mb-2">
                         <a href="#" class="block text-gray-800 hover:text-green-500 font-medium"
-                           :class="{'text-green-500': open}" @click="open = !open; show = false">
+                           :class="{'text-green-500': open}" @click="open = true; show = false">
                             Início
                         </a>
                     </li>
                     <li class="mb-2">
                         <a href="#" class="block text-gray-800 hover:text-green-500 font-medium"
-                           :class="{'text-green-500': show}" @click="show = !show; open = false"
+                           :class="{'text-green-500': show}" @click="show = true; open = false"
                         >Endpoint</a>
                     </li>
-                    {{--<li class="mb-2">
-                        <a href="#" class="block text-gray-800 hover:text-green-500 font-medium">Endpoint 3</a>
-                    </li>
-                    <!-- ... -->--}}
+
                 </ul>
             </nav>
 
-            <section class="w-full mx-auto lg:w-2/4 bg-white p-8 shadow rounded" x-cloak x-show="open">
+            <section class="w-full mx-auto bg-white p-8 shadow rounded" x-cloak x-show="open">
 
                 <div class="container mx-auto">
                     <h1 class="text-4xl font-bold mb-4 text-green-500 text-center">Documentação da API Viacnt</h1>
@@ -85,16 +82,17 @@
                             expandir suas funcionalidades.</p>
 
                         <h2 class="text-2xl font-semibold mb-2">Links Úteis</h2>
-                        <p class="mb-4"><span class="font-semibold">GitHub Repository:</span> <a
-                                href="https://github.com/Elivandro/cnt-api" class="text-blue-500 hover:underline">Link
-                                para o repositório no GitHub</a></p>
-                        {{--<p><span class="font-semibold">Documentação Completa:</span> <a href="seu-link-da-documentacao" class="text-blue-500 hover:underline">Link para a documentação detalhada</a></p>--}}
+                        <p class="mb-4"><span class="font-semibold">GitHub:</span> <a
+                                title="Link do Repositório no GitHub" target="_blank"
+                                href="https://github.com/Elivandro/cnt-api/blob/main/README.md"
+                                class="text-blue-500 hover:underline">Link
+                                para o repositório no GitHub </a></p>
                     </div>
                 </div>
             </section>
 
-            <section class="w-full mx-auto lg:w-2/4 bg-white p-8 shadow rounded" x-cloak x-show="show">
-                <div class="container mx-auto py-8">
+            <section class="w-full mx-auto bg-white p-8 shadow rounded" x-cloak x-show="show">
+                <div class="container mx-auto">
                     <h1 class="text-4xl font-bold mb-4 text-green-500 text-center">Documentação da API Viacnt</h1>
 
                     <div class="bg-white shadow p-6 rounded">
@@ -107,34 +105,95 @@
                             fornecendo o código CNAE desejado no formato de números:</p>
 
                         <div class="bg-gray-200 rounded p-4 mb-4">
-                            <p><span class="font-semibold">Endpoint:</span> <code>GET
-                                    /consultar-cnae?cnae={código}</code></p>
-                            <p><span class="font-semibold">Exemplo:</span> <code>/consultar-cnae?cnae=123456</code></p>
+                            <p><span class="font-semibold">Endpoint: </span>
+                                GET
+                                <span class="text-xs lg:text-sm"> https://viacnt.com.br/api/v1/cnt/6201500</span>
                         </div>
 
                         <h3 class="text-xl font-semibold mb-2">Resposta</h3>
                         <p class="mb-4">A resposta será um JSON contendo as informações do CNAE consultado, além dos
-                            códigos CNT correspondentes e suas descrições associadas.</p>
+                            códigos CNT correspondentes a suas descrições associadas.</p>
 
-                        <div class="bg-gray-200 rounded p-4">
-                <pre><code class="text-sm">{
-    "cnae": "123456",
-    "descricao_cnae": "Descrição do CNAE consultado...",
-    "codigos_cnt": [
-        {
-            "codigo": "CNT001",
-            "descricao": "Descrição do CNT001"
-        },
-        {
-            "codigo": "CNT002",
-            "descricao": "Descrição do CNT002"
-        },
-        ...
-    ]
-}</code></pre>
+                        <div class="bg-gray-200 h-fit rounded p-4 space-y-2 lg:space-x-0 lg:hidden">
+                            <div class="text-sm lg:text-sm">
+
+                                {
+                                "cnae_code": "6201-5/00",
+                                "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+                                "cnt": "01.01",
+                                "cnt_description": "Análise e desenvolvimento de sistemas."
+                                },
+                            </div>
+                            <div class="text-sm lg:text-sm">
+                                {
+                                "cnae_code": "6201-5/00",
+                                "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+                                "cnt": "01.02",
+                                "cnt_description": "Programação."
+                                },
+                            </div>
+                            <div class="text-sm lg:text-sm">
+                                {
+                                "cnae_code": "6201-5/00",
+                                "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+                                "cnt": "01.03",
+                                "cnt_description": "Processamento de dados e congêneres."
+                                },
+                            </div>
+                            <div class="text-sm lg:text-sm">
+                                {
+                                "cnae_code": "6201-5/00",
+                                "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+                                "cnt": "01.04",
+                                "cnt_description": "Elaboração de programas de computadores, inclusive de jogos
+                                eletrônicos."
+                                },
+                            </div>
+                            <div class="text-sm lg:text-sm">
+                                {
+                                "cnae_code": "6201-5/00",
+                                "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+                                "cnt": "01.08",
+                                "cnt_description": "Planejamento, confecção, manutenção e atualização de páginas
+                                eletrônicas."
+                                }
+                            </div>
+                        </div>
+                        <div class="bg-gray-200 rounded p-4 hidden lg:block">
+                <pre><code class="text-sm">  {
+            "cnae_code": "6201-5/00",
+            "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+            "cnt": "01.01",
+            "cnt_description": "Análise e desenvolvimento de sistemas."
+      },
+      {
+            "cnae_code": "6201-5/00",
+            "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+            "cnt": "01.02",
+            "cnt_description": "Programação."
+      },
+      {
+            "cnae_code": "6201-5/00",
+            "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+            "cnt": "01.03",
+            "cnt_description": "Processamento de dados e congêneres."
+      },
+      {
+            "cnae_code": "6201-5/00",
+            "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+            "cnt": "01.04",
+            "cnt_description": "Elaboração de programas de computadores, inclusive de jogos eletrônicos."
+      },
+      {
+            "cnae_code": "6201-5/00",
+            "cnae_description": "Desenvolvimento de programas de computador sob encomenda",
+            "cnt": "01.08",
+            "cnt_description": "Planejamento, confecção, manutenção e atualização de páginas eletrônicas."
+}
+</code></pre>
                         </div>
                     </div>
-                </div>
+
             </section>
         </div>
     </main>
