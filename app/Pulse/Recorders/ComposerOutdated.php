23edb75dff0e5711e71d505803657a56d3134fbb
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2024-01-20 10:11:38 -0400

    testando configurações 3

--- app/Pulse/Recorders/ComposerOutdated.php
+++ app/Pulse/Recorders/ComposerOutdated.php
@@ -5,22 +5,18 @@ namespace App\Pulse\Recorders;
 use Illuminate\Config\Repository;
 use Illuminate\Support\Facades\Process;
 use Laravel\Pulse\Events\SharedBeat;
-use Laravel\Pulse\Pulse;
 
 final class ComposerOutdated
 {
     public string $listen = SharedBeat::class;
 
-    public function __construct(protected Pulse $pulse, protected Repository $config)
+    public function __construct(protected $pulse, protected Repository $config)
     {
         //
     }
 
     public function record(SharedBeat $event): void
     {
-//        if($event->time !== $event->time->startOfDay()){
-//            return;
-//        }
 
         $outdated = Process::run('composer outdated -D -f json');
 
