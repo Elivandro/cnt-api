5ee103302e49d2e2ce8dd84e249921451aa5b5f3
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2024-01-29 13:47:40 -0400

    rf: retirado teste de senha

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => 'user@email.com',
             'email_verified_at' => now(),
-            'password' => 'password',//static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
