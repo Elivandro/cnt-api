d13441fe8b607ada5d3d4d888aaec4266e8dca12
Elivandro <Elivandro@hotmail.com.br>
2024-05-19 17:51:38 -0400

    dependencies updated

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,18 +7,23 @@ echo "Deployment started ..."
 git pull origin main
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+echo "Installing the composer dependencies"
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader --quiet
 
 # Install npm dependencies
-npm install -y
+echo "Installing the npm dependencies"
+npm install -y --silent
 
 # Recreate cache
+echo "Optimizing the cache"
 php artisan optimize
 
 # Compile npm assets
+echo "Compilling the npm assets"
 npm run build
 
 # Run database migrations
+echo "Running the migrations"
 php artisan migrate --force
 
 echo "Deployment finished!"
